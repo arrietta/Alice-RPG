6038188221eb5a08e2bc526a4805cb66ab21c16c
prodyozy <210103075@stu.sdu.edu.kz>
2024-05-15 18:15:27 +0500

    Map created

---
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    [SerializeField] private int speed = 5;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
